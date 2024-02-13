22b0a6aad90280dc8aaf36061284488f63492970
Nekopawa <rafaalinehoffman@gmail.com>
2024-02-13 09:47:08 -0300

    Fix profile.json url

--- assets/script/api.js
+++ assets/script/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-  const url = `../../data/profile.json`;
+  const url = "./data/profile.json";
   const data = await (await fetch(url)).json();
 
   return data;
